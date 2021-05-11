622191a9938e74da317f3020201f444c30c6ed20
SWH <slgo123@naver.com>
2021-05-11 18:41:01 +0900

    edit page

---
+++ src/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import "./Main.css"
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Simple 페이지</h1>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Main
