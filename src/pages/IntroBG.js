9d3d5873c614184d56ab37a338e4888038759fa4
SWH <slgo123@naver.com>
2021-05-16 22:44:08 +0900

    intro text done

--- src/pages/IntroBG.js
+++ src/pages/IntroBG.js
@@ -7,7 +7,13 @@ const IntroBG = () => {
   return (
     <section className="MainIntro-Section">
       <div className="MainIntro-wrapper">
-        <h1>Hi I'm Seok WooHyeon</h1>
+        <div className="intro-inner-wrapper">
+          <p className="iiw-f1">Hi I'm</p>
+          <p className="iiw-f2">
+            <label>seok</label> <label>w</label>oo<label>h</label>yeon
+          </p>
+          <p className="iiw-f3">Web developer</p>
+        </div>
       </div>
     </section>
   )
