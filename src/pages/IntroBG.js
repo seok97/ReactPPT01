95ef1effab6c108e72c8063d7cabf3d0629b7710
SWH <slgo123@naver.com>
2021-05-21 19:13:30 +0900

    0521

--- src/pages/IntroBG.js
+++ src/pages/IntroBG.js
@@ -20,10 +20,39 @@ const IntroBG = () => {
 
       <div className="MyInfoBox">
         <div className="mib-InnerWrapper">
-          <div className="mib-iw-Content">
-            <div className="mib-iw-con">
+          <div className="mib-iw-Content int-fonts">
+            <div className="mib-iw-con-about">
               <span>About me</span>
-              <div className="mib-iw-con-aboutme"></div>
+            </div>
+            <div className="mib-iw-con-introduce">
+              <p>
+                웹 개발자 석우현 입니다.
+                <br /> 혼자 웹 페이지 개발/배포 까지 해 보았으며
+                <br />
+                프론트엔드와 백엔드에 대해 잘 이해하고 있고 주로
+                <br />
+                프론트엔드 개발을 하고 있습니다. <br />
+                특히 Spring , ReactJS 프레임워크에 익숙 합니다.
+              </p>
+            </div>
+          </div>
+          <div className="mib-iw-con-skils int-fonts">
+            <div className="mib-iw-con-s">
+              <span>Skils</span>
+            </div>
+            <div className="mib-iw-con-s-w">
+              <div className="s-chart-f">
+                <span>Front-end</span>
+                <footer>
+                  <div className="chart"></div>
+                </footer>
+              </div>
+              <div className="s-chart-f">
+                <span>Back-end</span>
+                <footer>
+                  <div className="chart chart-b"></div>
+                </footer>
+              </div>
             </div>
           </div>
         </div>
