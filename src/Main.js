d32e042d66a83fa6b2774afd217f10c4f56f59d1
SWH <slgo123@naver.com>
2021-05-23 14:57:42 +0900

    main page done

--- src/Main.js
+++ src/Main.js
@@ -23,19 +23,25 @@ class Main extends Component {
             <div className="header_div">
               <div className="navbar">
                 <div className="nav_rogoimg">
-                  <Link to="/">
+                  <Link to="/" className="ac-deco">
                     <img src={rogoimg} alt="rogo_swh" />
                   </Link>
                 </div>
                 <ul className="header_ul">
                   <li className="header_ul_li">
-                    <Link to="/">Main</Link>
+                    <Link to="/" className="ac-deco">
+                      Main
+                    </Link>
                   </li>
                   <li className="header_ul_li">
-                    <Link to="/Skils">Skils</Link>
+                    <Link to="/Skils" className="ac-deco">
+                      Skils
+                    </Link>
                   </li>
                   <li className="header_ul_li">
-                    <Link to="/Projects">Projects</Link>
+                    <Link to="/Projects" className="ac-deco">
+                      Projects
+                    </Link>
                   </li>
                 </ul>
               </div>
@@ -50,12 +56,25 @@ class Main extends Component {
             </Switch>
           </section>
           <footer>
-            <h1>this is footer</h1>
-            <h3>create by SeokWooHyeon</h3>
-            <span>
-              {" "}
-              <a href="https://github.com/seok97/ReactPPT01">github</a>
-            </span>
+            <div className="Footer-wrapper">
+              <div className="f-w-contact">
+                <div className="f-w-c-span">
+                  <span>@: slgo1237@gmail.com</span>
+                </div>
+                <div className="f-w-c-span">
+                  <span>
+                    <a
+                      href="https://github.com/seok97/ReactPPT01"
+                      className="ac-deco">
+                      github
+                    </a>
+                  </span>
+                </div>
+              </div>
+              <div className="f-w-create">
+                <span>create by SeokWooHyeon</span>
+              </div>
+            </div>
           </footer>
         </div>
       </Router>
